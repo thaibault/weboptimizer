8023dcef56d3634d9ff1fe794a94acdc5d3ac65b
torben <t.sickert[~at~]gmail.com>
2016-03-04 12:02:03 +0100

    Automatic update to new version.

--- taskRunner.js
+++ taskRunner.js
@@ -27,7 +27,8 @@ if(global.process.argv[2] === 'clear')
         processOptions)
 else if(global.process.argv[2] === 'build')
     process = run(
-        `webpack --config ${__dirname}/webpack.config.js`, processOptions, (
+        `webpack --config ${__dirname}/webpack.config.js` +
+        packageConfiguration.commandLineArguments.webpack, processOptions, (
             error
         ) => {
             if(!error)
