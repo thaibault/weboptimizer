ae3ed44278c980b03f8ccc2aa7479bbd5517f8fd
torben <t.sickert[~at~]gmail.com>
2016-03-08 19:33:59 +0100

    Automatic update to new version.

--- configurator.js
+++ configurator.js
@@ -8,7 +8,7 @@ const specificConfiguration = require('../../package').webOptimizer || {}
 // endregion
 // NOTE: Given node command line arguments results in "npm_config_*"
 // environment variables.
-var debug = configuration.default.debug
+var debug = module.exports.default.debug
 if(typeof specificConfiguration.debug !== undefined)
     debug = specificConfiguration.debug
 if(global.process.env.npm_config_production)
