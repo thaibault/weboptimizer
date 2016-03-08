3e70906841ce12273ecdc9b7be80e1eca78a7d7c
torben <t.sickert[~at~]gmail.com>
2016-03-08 20:04:26 +0100

    Automatic update to new version.

--- configurator.js
+++ configurator.js
@@ -67,7 +67,9 @@ for (let key of [
     'sourcePath', 'targetPath', 'sourceAssetPath', 'targetAssetPath'
 ])
     if(module.exports[key])
-        module.exports[key] = path.normalize(`../../${module.exports[key]}`)
+        module.exports[key] = path.resolve(
+            __dirname, '../../', module.exports[key]
+        ) + '/'
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
 // vim: foldmethod=marker foldmarker=region,endregion:
