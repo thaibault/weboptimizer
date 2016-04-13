4fc2c60d7adf7fece278b364f3439eef28d65e1f
torben <t.sickert[~at~]gmail.com>
2016-04-13 11:00:29 +0200

    Automatic update to new version.

--- jadeLoader.js
+++ jadeLoader.js
@@ -4,6 +4,7 @@
 // region imports
 import extend from 'extend'
 import * as jade from 'jade'
+import * as jadeLoader from 'jade-loader'
 import * as loaderUtils from 'loader-utils'
 // NOTE: Only needed for debugging this file.
 try {
@@ -11,17 +12,32 @@ try {
 } catch (error) {}
 // endregion
 module.exports = function(source) {
-    this.cacheable()
+    if (this.cacheable)
+        this.cacheable()
     const query = extend(true, this.options.jade || {}, loaderUtils.parseQuery(
         this.query))
     const request = loaderUtils.getRemainingRequest(this).replace(/^!/, '')
-    const locals = query.locals
+    const locals = query.locals || {}
     delete query.locals
-    return jade.compile(source, extend(true, {
-        filename: request,
-        doctype: 'html',
-        compileDebug: this.debug || false
-    }, query))(locals)
+    const compile = (filePath=request, options=query) => {
+        return locals => {
+            return jade.compile(source, extend(true, {
+                filename: filePath, doctype: 'html',
+                compileDebug: this.debug || false
+            }, options))(extend(true, {require: request => {
+                this.resolve(this.context, request, (error, result) => {
+                    if (!error)
+                        this.addDependency(result)
+                    if (result.endsWith('.jade'))
+                        return compile(result)
+                    return result
+                })
+                // TODO
+                return 'TEST'
+            }}))
+        }
+    }
+    return compile()(locals)
 }
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
