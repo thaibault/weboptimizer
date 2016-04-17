1ad2819845cf1b00ddcccc54ec67c4d97dd5174b
torben <t.sickert[~at~]gmail.com>
2016-04-17 13:49:01 +0200

    Automatic update to new version.

--- helper.js
+++ helper.js
@@ -22,59 +22,46 @@ export default {
         // Checks if given entity is a function.
         return object && {}.toString.call(object) === '[object Function]'
     },
-    // NOTE: Uncomment this feature if node or babel supports es2015 proxies.
-    resolve: function(
-        object, currentConfiguration = null/* , initial = true*/
-    ) {
+    resolve: function(object, configuration = null, initial = true) {
         // Processes all dynamically linked values in given object.
-        if (!currentConfiguration)
-            currentConfiguration = object
-        // NOTE: Uncomment this feature if node or babel supports es2015
-        // proxies.
-        /*
+        if (!configuration)
+            configuration = object
         if (initial) {
             const attachProxy = object => {
                 for (let key in object)
                     if (this.isObject(object[key]))
                         attachProxy(object[key])
+                // NOTE: Replace this return code with outcomment code and
+                // remove all unneeded "resolve" calls in all potentially given
+                // configurations if node or babel supports proxies.
+                return object
+                /*
                 return new global.Proxy(object, {get: (target, name) => {
                     return this.resolve(
-                        target[name], currentConfiguration, false)
+                        target[name], configuration, false)
                 }})
+                */
             }
-            attachProxy(currentConfiguration)
+            attachProxy(configuration)
         }
-        */
         if (global.Array.isArray(object)) {
             let index = 0
             for (let value of object) {
-                object[index] = this.resolve(
-                    value, currentConfiguration, false)
+                object[index] = this.resolve(value, configuration, false)
                 index += 1
             }
         } else if (this.isObject(object))
             for (let key in object) {
-                if (key === '__execute__') {
-                    // NOTE: Change "let result = " to "return " if node or
-                    // babel supports
-                    let result = this.resolve(new global.Function(
+                if (key === '__execute__')
+                    return this.resolve(new global.Function(
                         'global', 'self', 'resolve', 'webOptimizerPath',
                         'currentPath', 'path', `return ${object[key]}`
                     )(
-                        global, currentConfiguration, this.resolve, __dirname,
+                        global, configuration, this.resolve, __dirname,
                         global.process.cwd(), path
-                    ), currentConfiguration, false)
-                    // NOTE: Removes this polyfill if node or babel supports
-                    // es2015 proxies.
-                    result.replace(/\{"__execute__": (.+?)\}/g, match => {
-                        console.log(arguments)
-                        return match[0]
-                    })
-                    return result
-                    //
-                }
+                    ), configuration, false)
                 object[key] = this.resolve(
-                    object[key], currentConfiguration, false)
+                    object[key], configuration, false)
             }
         return object
     },
