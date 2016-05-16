afc35e30b21174fff21b13ae467551864d3a5e90
torben <t.sickert[~at~]gmail.com>
2016-05-16 15:22:37 +0200

    Automatic update to new version.

--- webpackConfigurator.js
+++ webpackConfigurator.js
@@ -2,6 +2,17 @@
 // @flow
 // -*- coding: utf-8 -*-
 'use strict'
+/* !
+    region header
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons naming
+    3.0 unported license. see http://creativecommons.org/licenses/by/3.0/deed.de
+    endregion
+*/
 // region imports
 import * as fileSystem from 'fs'
 import * as dom from 'jsdom'
