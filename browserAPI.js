e2449eefba267a0acd1528d3dea99ff6f9fd6e7b
torben <t.sickert[~at~]gmail.com>
2016-08-06 10:14:02 +0200

    Automatic update to new version.

--- browserAPI.js
+++ browserAPI.js
@@ -41,6 +41,7 @@ if (typeof TARGET === 'undefined' || TARGET === 'node') {
         ].includes(error.type))
             console.warn(`Loading resource failed: ${error.toString()}.`)
         else
+            // IgnoreTypeCheck
             console.error(error.stack, error.detail)
     })
     let templateFilePath:string = path.join(__dirname, 'test.compiled.html')
