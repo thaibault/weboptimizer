27b04541318de73e1ff321ccc35119566de534c0
torben <t.sickert[~at~]gmail.com>
2016-08-10 20:01:15 +0200

    Automatic update to new version.

--- webpackConfigurator.js
+++ webpackConfigurator.js
@@ -71,6 +71,7 @@ let libraryName:string = configuration.exportFormat.self === 'var' ?
     Helper.convertToValidVariableName(configuration.name) : configuration.name
 if ('libraryName' in configuration)
     libraryName = configuration.libraryName
+console.log(libraryName)
 // // region plugins
 const pluginInstances:Array<Object> = [
     new webpack.optimize.OccurrenceOrderPlugin(true)]
