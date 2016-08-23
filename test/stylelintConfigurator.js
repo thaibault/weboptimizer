991bf710d0d0d07866956f05150fbc96d1c3a423
torben <t.sickert[~at~]gmail.com>
2016-08-23 14:20:47 +0200

    Automatic update to new version.

---
+++ test/stylelintConfigurator.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+// @flow
+// -*- coding: utf-8 -*-
+'use strict'
+// region imports
+import * as QUnit from 'qunit-cli'
+// NOTE: Only needed for debugging this file.
+try {
+    module.require('source-map-support/register')
+} catch (error) {}
+// endregion
+QUnit.module('stylelintkConfigurator')
+QUnit.load()
+// region tests
+QUnit.test('stylelintConfigurator', (assert:Object):void => {
+    assert.ok(require('../stylelintConfigurator.compiled').default)
+})
+// endregion
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
