66e5933ee5f68e7cf6ba004472d75b759b023200
torben <t.sickert[~at~]gmail.com>
2017-02-24 23:34:49 +0100

    Automatic update to new version.

--- configurator.js
+++ configurator.js
@@ -16,7 +16,6 @@
 // region imports
 import Tools from 'clientnode'
 import type {PlainObject} from 'clientnode'
-import ExtractText from 'extract-text-webpack-plugin'
 import * as fileSystem from 'fs'
 import path from 'path'
 // NOTE: Only needed for debugging this file.
@@ -364,8 +363,7 @@ for (
 resolvedConfiguration.module.aliases.webOptimizerDefaultTemplateFilePath$ =
     resolvedConfiguration.files.defaultHTML.template.filePath
 // endregion
-// region apply webpack plugin workarounds
-// / region html
+// region apply html webpack plugin workarounds
 /*
     NOTE: Provides a workaround to handle a bug with chained loader
     configurations.
@@ -393,7 +391,6 @@ for (
         htmlConfiguration.template.request = requestString
     }
 }
-// / endregion
 // endregion
 export default resolvedConfiguration
 // region vim modline
