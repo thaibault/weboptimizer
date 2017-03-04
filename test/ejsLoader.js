122a1faa48a93799f51433197c3f2dc5fb856399
torben <t.sickert[~at~]gmail.com>
2017-03-04 12:08:57 +0100

    Automatic update to new version.

---
+++ test/ejsLoader.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+// @flow
+// -*- coding: utf-8 -*-
+'use strict'
+// region imports
+import Tools from 'clientnode'
+import registerTest from 'clientnode/test.compiled'
+// NOTE: Only needed for debugging this file.
+try {
+    require('source-map-support/register')
+} catch (error) {}
+
+import ejsLoader from '../ejsLoader.compiled'
+// endregion
+registerTest(function():void {
+    this.module('ejsLoader')
+    //  region mockup
+    const context:{
+        addDependency:() => void;
+        options:{ejs?:{
+            locals?:Object;
+            compiler?:Object
+        }};
+        query:string;
+        debug:boolean;
+        loaders:Array<Object>;
+        loaderIndex:number
+    } = {
+        addDependency: ():void => {},
+        options: {},
+        query: '',
+        debug: true,
+        loaders: [],
+        loaderIndex: 0
+    }
+    // endregion
+    // region tests
+    this.test('loader', (assert:Object):void => {
+        assert.strictEqual(ejsLoader.call(context, 'a'), '<a></a>')
+        const complexContext = Tools.extendObject(true, {}, context, {
+            cacheable: ():void => {}, options: {
+                locals: {test: 'hans'},
+                compiler: {pretty: true}
+            }
+        })
+        assert.strictEqual(ejsLoader.call(
+            complexContext, 'a #{test}'
+        ), '<a>hans</a>')
+        assert.strictEqual(ejsLoader.call(
+            complexContext,
+            `a\n!= require('a test?{options: {isString: true}}')`
+        ), '<a></a><a>test</a>')
+    })
+    // endregion
+}, ['plain'])
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
