8e8624c276a51948b4d4a11d0689fb95e8b58ac5
torben <t.sickert[~at~]gmail.com>
2019-09-04 17:08:43 +0200

    Automatic update to new version.

--- test/browserAPI.js
+++ test/browserAPI.js
@@ -10,31 +10,37 @@ import browserAPI from '../browserAPI.compiled'
 import type {BrowserAPI} from '../type'
 // endregion
 registerTest(function():void {
-    browserAPI((api:BrowserAPI):void => {
-        this.module('browserAPI')
-        // region tests
-        this.test('browserAPI', (assert:Object):void => {
-            browserAPI((api:BrowserAPI, alreadyCreated:boolean):void =>
-                assert.ok(alreadyCreated))
-            assert.notOk(api.debug)
-            assert.ok(api.window.hasOwnProperty('document'))
-            assert.ok(api.window.document.hasOwnProperty('location'))
-            assert.ok(
-                api.window.document.querySelector('body') instanceof Object)
-            const done:Function = assert.async()
-            const timer:Promise<boolean> = Tools.timeout(done, 100)
-            api.window.document.addEventListener('DOMContentLoaded', (
-                event:Object
-            ):void => {
-                // IgnoreTypeCheck
-                timer.clear()
-                assert.ok(event)
-                done()
+    browserAPI(
+        (api:BrowserAPI):void => {
+            this.module('browserAPI')
+            // region tests
+            /*this.*/test('browserAPI', (assert:Object):void => {
+                browserAPI((api:BrowserAPI, alreadyCreated:boolean):void =>
+                    assert.ok(alreadyCreated))
+                assert.notOk(api.debug)
+                assert.ok(api.window.hasOwnProperty('document'))
+                assert.ok(api.window.document.hasOwnProperty('location'))
+                assert.ok(
+                    api.window.document.querySelector('body') instanceof
+                    Object
+                )
+                const done:Function = assert.async()
+                const timer:Promise<boolean> = Tools.timeout(done, 100)
+                api.window.document.addEventListener(
+                    'DOMContentLoaded',
+                    (event:Object):void => {
+                        // IgnoreTypeCheck
+                        timer.clear()
+                        assert.ok(event)
+                        done()
+                    }
+                )
             })
-        })
-        // endregion
-    }, false)
-}, ['plain'])
+            // endregion
+        },
+        false
+    )
+}, 'plain')
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
 // vim: foldmethod=marker foldmarker=region,endregion:
