3440c68c5f9a4200d95900dac3e58701f6601cb5
torben <t.sickert[~at~]gmail.com>
2021-02-02 13:00:50 +0100

    Automatic update to new version.

--- ejsLoader.ts
+++ ejsLoader.ts
@@ -291,7 +291,9 @@ export default function(this:any, source:string):string {
                 }
             } else
                 result = compressHTML(!options.strict && options._with ?
-                    result(scope!, scope!.escapeFn, scope!.include) :
+                    (result as Function)(
+                        scope!, scope!.escapeFn, scope!.include
+                    ) :
                     result(
                         /*
                             NOTE: We want to be ensure to have same ordering as
