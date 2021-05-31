c95e701242ef7abd37dc7a4f892a2cc03a9c42f9
torben <t.sickert[~at~]gmail.com>
2021-05-31 22:42:24 +0200

    Automatic update to new version.

--- test/browser.ts
+++ test/browser.ts
@@ -5,35 +5,36 @@
 import getInitializedBrowser, {browser} from '../browser'
 import {Browser} from '../type'
 // endregion
-test('browser', async (done:Function):Promise<void> => {
+test('browser', (done:Function):void => {
     expect(browser.debug).toStrictEqual(false)
     expect(browser.domContentLoaded).toStrictEqual(false)
     expect(browser.initialized).toStrictEqual(false)
     expect(browser.windowLoaded).toStrictEqual(false)
 
-    const initializedBrowser:Browser = await getInitializedBrowser()
-    expect(initializedBrowser).toStrictEqual(browser)
-    expect(browser.initialized).toStrictEqual(true)
-    expect(browser.window).toHaveProperty('document')
+    getInitializedBrowser().then((initializedBrowser:Browser):void => {
+        expect(initializedBrowser).toStrictEqual(browser)
+        expect(browser.initialized).toStrictEqual(true)
+        expect(browser.window).toHaveProperty('document')
 
-    if (browser.window)
-        expect(browser.window.document).toHaveProperty('location')
-    const onWindowLoaded = (event:Event):void => {
-        expect(event).toBeInstanceOf(Object)
-        expect(browser.domContentLoaded).toStrictEqual(true)
         if (browser.window)
-            expect(browser.window.document.querySelector('body'))
-                .toBeInstanceOf(Object)
-        expect(browser.windowLoaded).toStrictEqual(true)
-        done()
-    }
+            expect(browser.window.document).toHaveProperty('location')
+        const onWindowLoaded = (event:Event):void => {
+            expect(event).toBeInstanceOf(Object)
+            expect(browser.domContentLoaded).toStrictEqual(true)
+            if (browser.window)
+                expect(browser.window.document.querySelector('body'))
+                    .toBeInstanceOf(Object)
+            expect(browser.windowLoaded).toStrictEqual(true)
+            done()
+        }
 
-    if (browser.windowLoaded)
-        onWindowLoaded(new Event('load'))
-    else if (browser.window)
-        browser.window.addEventListener('load', onWindowLoaded)
-    else
-        done()
+        if (browser.windowLoaded)
+            onWindowLoaded(new Event('load'))
+        else if (browser.window)
+            browser.window.addEventListener('load', onWindowLoaded)
+        else
+            done()
+    })
 })
 // region vim modline
 // vim: set tabstop=4 shiftwidth=4 expandtab:
