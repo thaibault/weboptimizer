58d573dfbefa7268f9fe625933a2f5b5c4f3a1bc
torben <t.sickert[~at~]gmail.com>
2022-04-30 14:07:56 +0200

    Automatic update to new version.

---
+++ jestSetup.ts
@@ -0,0 +1,27 @@
+// #!/usr/bin/env babel-node
+// -*- coding: utf-8 -*-
+/** @module webpackConfigurator */
+'use strict'
+/* !
+    region header
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons
+    naming 3.0 unported license.
+    See https://creativecommons.org/licenses/by/3.0/deed.de
+    endregion
+*/
+// region imports
+import {TextEncoder, TextDecoder} from 'util'
+// endregion
+module.exports = async function (globalConfig, projectConfig) {
+    global.TextEncoder = TextEncoder
+    global.TextDecoder = TextDecoder 
+}
+// region vim modline
+// vim: set tabstop=4 shiftwidth=4 expandtab:
+// vim: foldmethod=marker foldmarker=region,endregion:
+// endregion
